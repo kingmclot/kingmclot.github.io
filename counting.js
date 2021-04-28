bebebf4dea292a39dafdf993b1a251f7bd917bb0
kingmclot <81751649+kingmclot@users.noreply.github.com>
2021-04-28 16:38:10 +0900

    Correct typo

--- counting.js
+++ counting.js
@@ -1,9 +1,9 @@
 const counting = function (json) {
-    const count_per_destrict = new Object();
+    const count_per_district = new Object();
     let features = json.features;
     for(let i=0; i<features.length; i++) {
         let district_code = features[i]["properties"]["district_code"];
-        count_per_destrict[district_code] ? count_per_destrict[district_code] += 1 : count_per_destrict[district_code] = 1
+        count_per_district[district_code] ? count_per_district[district_code] += 1 : count_per_district[district_code] = 1
     } 
-    return count_per_destrict;
+    return count_per_district;
 }
